f114f6db3a63a43f6e07b84b33500bc67119fd0d
himanshu <himanshu.ajwani96@gmail.com>
2024-02-08 01:11:14 +0530

    other operation

--- src/Main.java
+++ src/Main.java
@@ -186,7 +186,6 @@ class Calculator {
 
 }
 
-
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
